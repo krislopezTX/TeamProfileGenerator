a82dce4950946fce82b9452a104836dc08be7a2e
Kris Lopez <krislopeztx@yahoo.com>
2022-01-31 14:57:33 -0600

    updated test files

---
+++ _test_/Engineer.test.js
@@ -0,0 +1,8 @@
+const { TestWatcher } = require('jest');
+const Engineer = require('../lib/Engineer');
+
+test('creates Engineer', () => {
+    const engineer = new Engineer('Kris', '15', 'krislopez68@yahoo.com');
+    
+
+})
